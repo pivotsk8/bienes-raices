cfe82e01b7ee8d94eeafe86eec3d0a37666acf6c
piiivot <the_pivotsk8@hotmail.com>
2023-07-12 09:49:21 -0500

    firts commit

---
+++ src/main.js
@@ -0,0 +1,27 @@
+import './assets/main.css';
+
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+
+import App from './App.vue';
+import router from './router';
+
+//vuetify
+import 'vuetify/styles';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+
+const app = createApp(App);
+
+//vuetify
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+
+app.use(vuetify);
+app.use(createPinia());
+app.use(router);
+
+app.mount('#app');
---
+++ src/views/HomeView.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+
+<template>
+  <h1 class="text-center">hola</h1>
+</template>
