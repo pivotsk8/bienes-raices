ad6f68c74368aadf42d1bd7c476f3c9223c1a627
piiivot <the_pivotsk8@hotmail.com>
2023-07-12 10:22:11 -0500

    FEAT routeo de inicio y registro

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,25 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router';
 </script>
 
 <template>
+  <VCard elevation="3" max-width="1200" class="mx-auto">
+    <VLayout>
+      <v-app-bar color="blue-darken-1">
+        <template v-slot:prepend>
+          <VBtn :to="{ name: 'home' }">Bienes-Raices</VBtn>
+        </template>
+        <template v-slot:append>
+          <VBtn :to="{ name: 'home' }">Inicio</VBtn>
+          <VBtn :to="{ name: 'login' }">Iniciar Sesaión</VBtn>
+        </template>
+      </v-app-bar>
 
-
-  <RouterView />
+      <VMain>
+        <VContainer>
+          <RouterView />
+        </VContainer>
+      </VMain>
+    </VLayout>
+  </VCard>
 </template>
-
-
---
+++ src/views/LoginView.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+
+<template>
+  <h1 class="text-center text-h3 font-weigth-bold my-5">Iniciar Sesión</h1>
+</template>
