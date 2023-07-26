06feb66622ce6ac743f97132d757dfa9a3f379fe
piiivot <the_pivotsk8@hotmail.com>
2023-07-26 16:35:03 -0500

    feat: Se agrego storage para imagen y se agrego la url a la DB

--- src/composables/useImage.js
+++ src/composables/useImage.js
@@ -1,3 +1,4 @@
+import { computed } from 'vue';
 import { ref as storageRef } from 'firebase/storage';
 import { useFirebaseStorage, useStorageFile } from 'vuefire';
 import { uid } from 'uid';
@@ -14,7 +15,13 @@ export default function useImage() {
     data ? upload(data) : null;
   }
 
+  const image = computed(() => {
+    return url.value ? url.value : null;
+  });
+
   return {
+    url,
     uploadImage,
+    image,
   };
 }
